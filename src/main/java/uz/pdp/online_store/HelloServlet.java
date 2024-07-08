63d5603bf03ff996b7ef7872123ea80c625873d8
tolik <pdp@gmail.com>
2024-07-08 10:49:09 +0500

    test commit

--- src/main/java/uz/pdp/online_store/HelloServlet.java
+++ src/main/java/uz/pdp/online_store/HelloServlet.java
@@ -21,6 +21,7 @@ public class HelloServlet extends HttpServlet {
         out.println("<html><body>");
         out.println("<h1>" + message + "</h1>");
         out.println("</body></html>");
+        /*for commit*/
     }
 
     public void destroy() {
